78ef3d0421bcc7fa21c099a1153603a500b5b905
James Conant <james@conant.io>
2018-07-04 00:51:29 -0400

    Local testing

--- lib/stream_ci/ruby/rspec/runner.rb
+++ lib/stream_ci/ruby/rspec/runner.rb
@@ -1,5 +1,7 @@
 require 'rspec/core'
 require 'httparty'
+require 'json'
+require 'pry'
 
 module StreamCi
   module Ruby
@@ -10,13 +12,31 @@ module StreamCi
           @configuration.output_stream = out if @configuration.output_stream == $stdout
           @options.configure(@configuration)
 
-          root_path = StreamCi::Ruby::Rspec.root
-          test_manifest = Dir["#{root_path}/**/*_spec.rb"].map do |fp|
-            fp.gsub("#{root_path}/", '')
+          @configuration.output_stream.print(
+            "#\n# Preparing test manifest to send to StreamCI\n#\n"
+          )
+          t1 = Time.now
+
+          project_root = Rails.root.to_s # this needs to be setup via a config option, as non-rails apps might use this
+          spec_root = '/spec' # pull from rspec config?
+          test_manifest = Dir["#{project_root}#{spec_root}/**/*_spec.rb"].map do |fp|
+            fp.gsub("#{project_root}/", '')
           end
 
-          opts = { query: { api_key: '12345', branch: 'test', build: '1', test_manifest: test_manifest } }
-          HTTParty.post('https://api.streamci.com/v1/tests/next', opts)
+          opts = {
+            query: {
+              api_key: '12345',
+              branch: 'test',
+              build: '1',
+              test_manifest: test_manifest & @options.args
+            }
+          }
+
+          stream_ci_url = ENV['STREAM_CI_URL'] || 'https://api.streamci.com'
+          HTTParty.post("#{stream_ci_url}/v1/tests", opts)
+
+          t2 = Time.now
+          @configuration.output_stream.print "#\n# Test manifest sent to StreamCI - (#{((t2 - t1) * 1000).round} ms)\n#\n"
 
           # TODO
           # * What do these commands do / mean?
@@ -36,15 +56,20 @@ module StreamCi
               @no_failures = true
 
               opts = { query: { api_key: '12345', branch: 'test', build: '1' } }
-              response = HTTParty.get('https://api.streamci.com/v1/tests/next', opts)
+              stream_ci_url = ENV['STREAM_CI_URL'] || 'https://api.streamci.com'
+              response = HTTParty.get("#{stream_ci_url}/v1/tests/next", opts)
 
+              binding.pry
               until response.code == 204 || response.code >= 300 do
                 # should we clear the world / example groups before each one?
                 # will that mess up reporting?
                 #
                 # does this need to be the full path or just spec/some/spec/path ?
+                binding.pry
                 if response.code == 200
-                  load response.body
+                  puts "Running #{response.body}"
+                  binding.pry
+                  load JSON.parse(response.body)['test']
                   unless @world.example_groups.last.run(reporter)
                     @no_failures = false
                   end
--- lib/stream_ci/ruby/rspec/version.rb
+++ lib/stream_ci/ruby/rspec/version.rb
@@ -1,7 +1,7 @@
 module StreamCi
   module Ruby
     module Rspec
-      VERSION = '0.8.0'
+      VERSION = '0.9.0'
     end
   end
 end
